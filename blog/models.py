8e851c68ae22402aff59f111c0fa08cc567f7549
leonid Petrov <leonidpetrov637@gmail.com>
2024-05-17 00:44:24 +0300

    Created a new Blog model and described CRUD-based controllers for it

---
+++ blog/models.py
@@ -0,0 +1,54 @@
+from django.db import models
+
+
+NULLABLE = {"blank": True, "null": True}
+
+
+class Blog(models.Model):
+    title = models.CharField(
+        max_length=60,
+        verbose_name="Заголовок",
+        help_text="Введите заголовок",
+        default=None
+    )
+    slug = models.CharField(
+        max_length=60,
+        verbose_name='slug',
+        help_text='Введите slug',
+        **NULLABLE
+    )
+    content = models.TextField(
+        verbose_name="Текст",
+        help_text="Введите текст",
+        **NULLABLE
+    )
+    photo = models.ImageField(
+        upload_to="blogs/",
+        verbose_name="Фото",
+        help_text="Загрузите фото",
+        **NULLABLE
+    )
+    created_at = models.DateField(
+        verbose_name="Дата создания",
+        help_text="Укажите дату создания",
+        **NULLABLE
+    )
+    is_published = models.BooleanField(
+        verbose_name="Публикация",
+        help_text="Укажите статус публикации",
+        default=True,
+        **NULLABLE
+    )
+    views_count = models.IntegerField(
+        verbose_name="Просмотры",
+        help_text="Количество просмотров",
+        default=0,
+        **NULLABLE
+    )
+
+    def __str__(self):
+        return self.title
+
+    class Meta:
+        verbose_name = "Блог"
+        verbose_name_plural = "Блоги"
