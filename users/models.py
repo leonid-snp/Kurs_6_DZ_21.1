b61ca69d623ace9e2376e333c25316d6906f1f3c
leonid Petrov <leonidpetrov637@gmail.com>
2024-06-04 14:11:02 +0300

    user model

--- users/models.py
+++ users/models.py
@@ -3,7 +3,7 @@ from django.db import models
 from catalog.models import NULLABLE
 
 
-class Users(AbstractUser):
+class User(AbstractUser):
     name = None
     email = models.EmailField(
         unique=True,
