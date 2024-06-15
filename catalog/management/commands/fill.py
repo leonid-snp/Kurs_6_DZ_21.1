8b0c1616d3d6a4ffda280dbb4e1390547e768e69
leonid Petrov <leonidpetrov637@gmail.com>
2024-06-15 08:55:26 +0300

    made a division according to access rights, there is a group of moderators and content managers

--- catalog/management/commands/fill.py
+++ catalog/management/commands/fill.py
@@ -20,33 +20,14 @@ class Command(BaseCommand):
         list_products = [item.get("fields") for item in file if item.get("model") == "catalog.product"]
         return list_products
 
-    @staticmethod
-    def json_read_blog():
-        file = open_fixture('blog.json')
-        list_blogs = [item.get("fields") for item in file]
-        return list_blogs
-
-    @staticmethod
-    def json_read_users():
-        file = open_fixture('users.json')
-        list_users = [item.get("fields") for item in file]
-        return list_users
-
     def handle(self, *args, **options):
         Product.objects.all().delete()
         Category.objects.all().delete()
-        User.objects.all().delete()
-        Blog.objects.all().delete()
-        print(self.json_read_users())
 
         category_for_create = [Category(**item) for item in self.json_read_categories()]
-        users_for_create = [User(**item) for item in self.json_read_users()]
-        blog_for_create = [Blog(**item) for item in self.json_read_blog()]
         product_for_create = []
 
         Category.objects.bulk_create(category_for_create)
-        User.objects.bulk_create(users_for_create)
-        Blog.objects.bulk_create(blog_for_create)
 
         for product in Command.json_read_products():
             product_for_create.append(
