a20cfc583fe5e9d12ec6fbdb95b1db9d5757a7ab
leonid Petrov <leonidpetrov637@gmail.com>
2024-06-25 11:40:41 +0300

    cached the output of an individual product and a list of products, and removed the secret data into a separate file

--- blog/functions/utils.py
+++
@@ -1,7 +0,0 @@
-import environ
-
-
-def get_db_login(keys):
-    env = environ.Env()
-    environ.Env.read_env()
-    return env(keys)
--- catalog/urls.py
+++ catalog/urls.py
@@ -1,4 +1,6 @@
 from django.urls import path
+from django.views.decorators.cache import cache_page
+
 from catalog.apps import CatalogConfig
 from catalog.views import ProductCreateView, ProductListView, \
     ProductDetailListView, ContactListView, ContactCreateView, HomeTemplateView, ProductUpdateView
@@ -10,7 +12,7 @@ urlpatterns = [
     path('', ProductListView.as_view(), name='page_1'),
     path('contacts/', ContactListView.as_view(), name='contacts_list'),
     path('support/', ContactCreateView.as_view(), name='support'),
-    path('detail/<int:pk>/', ProductDetailListView.as_view(), name='detail_product'),
+    path('detail/<int:pk>/', cache_page(60)(ProductDetailListView.as_view()), name='detail_product'),
     path('add_product/', ProductCreateView.as_view(), name='create_product'),
     path('edit_product/<int:pk>/', ProductUpdateView.as_view(), name='edit_product'),
 ]
